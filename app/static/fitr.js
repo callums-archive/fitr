cfb5b589787434a554498825cb44521439660e1b
Callum Fleming <howzitcallum@gmail.com>
2019-07-10 14:15:35 +0200

    form looks complete

--- app/static/fitr.js
+++ app/static/fitr.js
@@ -1,3 +1,4 @@
+// cast form to json
 function castJson(arr) {
   $json_object = {};
 
@@ -21,3 +22,34 @@ function castJson(arr) {
     return JSON.stringify(jsonData);
   };
 })(jQuery);
+
+// Datepicker inline
+var datepicker_inline = function(el, format = "YYYY-MM-DD") {
+  this.init(el, format);
+}
+
+datepicker_inline.prototype = {
+  init: function(el, format) {
+    if ($(el).attr('id') != undefined) {
+      this.name = $(el).attr('id');
+    } else if ($(el).attr('name') != undefined) {
+      this.name = $(el).attr('name');
+    }
+
+    this.input = document.createElement("input");
+    this.input.setAttribute("name", this.name);
+    this.input.setAttribute("type", "text");
+    this.input.setAttribute("value", "");
+    this.input.style.display = "none";
+    this.input.style.disabled = true;
+    this.input.classList = "form-control";
+    this.input.value = $(el).data("DateTimePicker").date().format(format);
+
+    $(el).append(this.input);
+
+    var root = this;
+    $(el).on('dp.change', function(e) {
+      root.input.value = e.date.format(format);
+    });
+  }
+};
