0eebee08ac56cfcff384c509b5ba0b510995e043
Callum Fleming <howzitcallum@gmail.com>
2019-07-11 22:31:04 +0200

    uri v2

--- app/__init__.py
+++ app/__init__.py
@@ -26,13 +26,13 @@ def create_app():
     # config
     app.config.from_json('config.json')
 
-    # app.config['MONGODB_SETTINGS'] = {
-    #   "db": "fitr",
-    #   "host": "35.247.114.174",
-    #   "port": 27017,
-    #   "username": "fitr",
-    #   "password": "4szlBDVZFOhhfACsIlZk10M5vmhTEbwCrv6f0zFV5NY="
-    # }
+    app.config['MONGODB_SETTINGS'] = {
+      "db": "fitr",
+      "host": "mongodb://35.247.114.174:27017/fitr",
+      "port": 27017,
+      "username": "fitr",
+      "password": "4szlBDVZFOhhfACsIlZk10M5vmhTEbwCrv6f0zFV5NY="
+    }
 
 
 
