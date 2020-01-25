caa84d3a574833ff079a65d99dd4c523e8dc794f
Callum Fleming <howzitcallum@gmail.com>
2020-01-25 13:56:57 +0200

    add clients interface and API coming on

--- fitr_webapp/views/trainer/__init__.py
+++ fitr_webapp/views/trainer/__init__.py
@@ -1,12 +1,18 @@
 from .capture import PTCaptureView
 from .manage_clients import *
+from .add_clients import *
 
 
 def register_views(app, base):
     PTCaptureView.register(app, route_base=base)
     # interface for datatable
     TrainerManageClients.register(app, route_base=base)
+
     # datatable for trainer clients
     TrainerManageClientsDataTable.register(app, route_base=base)
+
     # client management api
     TrainerManageClientAPI.register(app, route_base=base)
+
+    # add clients
+    TrainerAddClientsAPI.register(app, route_base=f"{base}/add")
